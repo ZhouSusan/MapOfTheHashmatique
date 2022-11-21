6f9f5546f08635294930f0d75ca23ee8c800730a
ZhouSusan <szhou089@gmail.com>
2022-11-20 23:08:57 -0800

    added TrackList class with attribute and constructor in TrackList.java and refactored package name to TeignmouthTrackList

--- src/main/java/TeignmouthTrackList/Main.java
+++ src/main/java/TeignmouthTrackList/Main.java
@@ -1,4 +1,4 @@
-package TeignmouthHashMap;
+package TeignmouthTrackList;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/TeignmouthTrackList/TrackList.java
@@ -0,0 +1,14 @@
+package TeignmouthTrackList;
+import java.util.HashMap;
+
+public class TrackList {
+    private HashMap<String, String> songMap;
+
+    public TrackList() {
+        this.songMap = new HashMap<String, String> ();
+        this.songMap.put("Tired To Death", "feel the power in the air bloody bodies running down the street try it again please it again");
+        this.songMap.put("Very Ill", "I wonder whether the girls are mad, And I wonder whether they mean to kill, And I wonder if William Bond will die, For assuredly he is very ill");
+        this.songMap.put("Wild World", "Feel the wild world in the air, Leddy body's running down the sraight, Try it again, Please it again, Can't control my mind, Can't control my mind, Feel the wild world in the air, You must be very ill, When you be lackey, Feel the wild world");
+        this.songMap.put("Fiorina", "My journey will not end, Something compells me, I travel through the darkness To be by your side");
+    }
+}
